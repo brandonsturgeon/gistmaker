63131b11d6c3f1cb2be8f6fdbbf69b6fe01e6155
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-26 15:29:57 -0700

    Restructure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gistmaker',
+  packages = ['gistmaker'],
+  version = '1.0.0',
+  description = 'An extremely simple python module used to programatically create Gists',
+  author = 'Brandon Sturgeon',
+  author_email = 'brandon@brandonsturgeon.com',
+  url = 'https://github.com/brandonsturgeon/gistmaker',
+  download_url = 'https://github.com/brandonsturgeon/gistmaker/archive/1.0.0.tar.gz',
+  keywords = ['gist', 'maker', 'creator', 'create'],
+  classifiers = [],
+)
